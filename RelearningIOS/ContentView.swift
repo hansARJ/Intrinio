424bf949c7ddda1d297138d7e4491f00e26ad167
Hans Arijanto <hansarijanto@hotmail.com>
2020-01-25 20:55:33 -0500

    Initial Commit

---
+++ RelearningIOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RelearningIOS
+//
+//  Created by Hans Arijanto on 2020-01-25.
+//  Copyright © 2020 Hans Arijanto. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
